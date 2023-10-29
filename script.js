92890532c3e818139dc799bbeba06295e2bfccbd
kachkynbaeva <kunduz1604@icloud.com>
2023-10-29 15:02:40 +0600

    finished

---
+++ script.js
@@ -0,0 +1,48 @@
+// let a = 'hello'
+// let b = 'ivan'
+//
+// console.log(a.slice(0,1).toUpperCase() +a.slice(1) + ' ' +(b.toLowerCase()))
+// let a = 'lorem ipsun dolor'
+// let b = a
+// console.log(b)
+// let a = `lorem ispun dolor sit amet`
+// let b =a.slice(18)
+// console.log(b)
+// let a = `lorem ispun dolor sit amet`
+// let b = a.slice( 18,21)
+// let c = a.slice(0,19)
+// let d = a.slice(21,26)
+// console.log(b + ` ` + c + d )
+// let a = `children`
+// let b = a.slice(0,7).toUpperCase()
+// let c =a.slice(7,8)
+// console.log(b+c)
+// let a = `boss`
+// let b = a.slice(0,1)+ a.slice(2)+a.slice(1,2)
+// console.log(b)
+// let a = `sunday`
+// let b = a.indexOf(`d`)
+// console.log(b)
+// let a = `user`
+// let b = `new`
+// let c = b.slice(0,3)+ ' '+a.slice(0,2)
+// console.log(c)
+// let a = `monday`
+// let b = `day`
+// let c = a.slice(0,3)
+// console.log(b+b+c)
+// let a = `HELLO`
+// let b = a.slice(0,1).toLowerCase()+a.slice(1,2).toUpperCase()
+// let c = a.slice(2,3)+a.replace(`L`, `I`).toUpperCase()
+// let d = a.slice(4,5).toLowerCase()
+// console.log( b + c + d)
+// let a =5
+// let b = `400`
+// let c = b.slice(0,1)+ a + b.slice(2,3)
+// console.log(c)
+// let a = `monkey`
+// let b = `banana`
+// let c = a.slice(0,1)+b.slice(1,4)+a.slice(3,4)
+// console.log(c)
+
+
